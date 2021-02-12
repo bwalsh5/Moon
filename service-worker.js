2918468fa7e8c498b563bfbac8872748954216cd
= <=>
2021-02-12 09:49:09 -0500

    new cache

--- service-worker.js
+++ service-worker.js
@@ -1,86 +1,32 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+// use a cacheName for cache versioning
+var cacheName = 'v1:static';
 
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
-
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  'index.html',
-  './', // Alias for index.html
-  'styles/main.css',
-  'scripts/main.js',
-  'images/moon/Moon_phase_0.svg',
-  'images/moon/Moon_phase_1.svg',
-  'images/moon/Moon_phase_2.svg',
-  'images/moon/Moon_phase_3.svg',
-  'images/moon/Moon_phase_4.svg',
-  'images/moon/Moon_phase_5.svg',
-  'images/moon/Moon_phase_6.svg',
-  'images/moon/Moon_phase_7.svg',
-  'error.html'
-];
-
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
-});
-
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-
-        if (cachedResponse) {
-          return cachedResponse;
-
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
+// during the install phase you usually want to cache static assets
+self.addEventListener('install', function(e) {
+    // once the SW is installed, go ahead and fetch the resources to make this work offline
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll([
+                './',
+                './error.html'
+            ]).then(function() {
+                self.skipWaiting();
             });
-          });
-        });
-      })
+        })
     );
-  }
-  return caches.match('/error.html');
+});
+
+// when the browser fetches a url
+self.addEventListener('fetch', function(event) {
+    // either respond with the cached object or go ahead and fetch the actual url
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                // retrieve from cache
+                return response;
+            }
+            // fetch as normal
+            return fetch(event.request);
+        })
+    );
 });
